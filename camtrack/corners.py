64d913a7fe04c39175ef74b3bd64a21ccb4e5cd6
Makhnev Konstantin <mah.kons@gmail.com>
2021-02-06 00:45:08 +0300

    corners tracking added

--- camtrack/corners.py
+++ camtrack/corners.py
@@ -17,6 +17,8 @@ import cv2
 import numpy as np
 import pims
 
+import itertools
+
 from _corners import (
     FrameCorners,
     CornerStorage,
@@ -48,16 +50,43 @@ class _CornerStorageBuilder:
 
 def _build_impl(frame_sequence: pims.FramesSequence,
                 builder: _CornerStorageBuilder) -> None:
-    # TODO
+    MAX_CORNERS = 5000
+    BLOCK_SIZE = 7
+    MIN_DISTANCE = 11
+    QUALITY_LEVEL = 0.01
+
     image_0 = frame_sequence[0]
-    corners = FrameCorners(
-        np.array([0]),
-        np.array([[0, 0]]),
-        np.array([55])
-    )
-    builder.set_corners_at_frame(0, corners)
+    corners = cv2.goodFeaturesToTrack(image_0, maxCorners=MAX_CORNERS, qualityLevel=QUALITY_LEVEL,
+            minDistance=MIN_DISTANCE, blockSize=BLOCK_SIZE, useHarrisDetector=False).squeeze(1)
+
+    ids_generator = itertools.count()
+    ids = np.array([next(ids_generator) for _ in range(len(corners))])
+    frame_corners = FrameCorners(ids, corners, np.ones_like(ids) * BLOCK_SIZE)
+
+    builder.set_corners_at_frame(0, frame_corners)
     for frame, image_1 in enumerate(frame_sequence[1:], 1):
-        builder.set_corners_at_frame(frame, corners)
+        corners, status, error = cv2.calcOpticalFlowPyrLK((image_0 * 255).astype(np.uint8),
+                (image_1 * 255).astype(np.uint8), corners, None, winSize=(15, 15), maxLevel=3, minEigThreshold=0.001)
+        status = status.squeeze(1)
+
+        corners = corners[status == 1]
+        ids = ids[status == 1]
+
+        new_corners = cv2.goodFeaturesToTrack(image_1, maxCorners=MAX_CORNERS, qualityLevel=QUALITY_LEVEL,
+                minDistance=MIN_DISTANCE, corners=None, mask=None, blockSize=BLOCK_SIZE, useHarrisDetector=False)
+        if new_corners is None:
+            new_corners = np.empty((0, 2), corners.dtype)
+        else:
+            new_corners = new_corners.squeeze(1)
+
+        dist = np.linalg.norm(corners[None, :] - new_corners[:, None], axis=2)
+        new_corners = new_corners[np.min(dist, axis=1) >= MIN_DISTANCE, :]
+
+        corners = np.concatenate((corners, new_corners))
+        ids = np.concatenate((ids, np.array([next(ids_generator) for _ in range(len(new_corners))], dtype=np.int32)))
+
+        frame_corners = FrameCorners(ids, corners, np.ones_like(ids) * BLOCK_SIZE)
+        builder.set_corners_at_frame(frame, frame_corners)
         image_0 = image_1
 
 
